4620edcdfe2df8d082599683804060bd88f361fc
simon_b <braunstein@stud.tu-darmstadt.de>
2014-05-22 13:36:11 +0200

    Dumb implementation of sectioning a image.

--- app/src/main/java/tud/tk3/ambientdisplay/app/display/Display.java
+++ app/src/main/java/tud/tk3/ambientdisplay/app/display/Display.java
@@ -3,7 +3,7 @@ package tud.tk3.ambientdisplay.app.display;
 /**
  * Created by martin on 21.05.14.
  */
-public class Display {
+public class Display implements Comparable<Display>{
 
     public String uid;
 
@@ -17,5 +17,18 @@ public class Display {
         this.width = width;
         this.height = height;
         this.dpi = dpi;
+    }
+
+    public int getSize() {
+        return width * height;
+    }
+
+    @Override
+    public int compareTo(Display display) {
+        int cmp = getSize() - display.getSize();
+        if(cmp == 0){
+            return uid.compareTo(display.uid);
+        }
+        return cmp;
     }
 }
