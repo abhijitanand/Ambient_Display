5810079524f3fb870f11c3c42a3691424c6006f5
simon_b <braunstein@stud.tu-darmstadt.de>
2014-05-22 18:17:29 +0200

    AmbientDiplay Interfave extended.

--- app/src/main/java/tud/tk3/ambientdisplay/app/communication/AmbientDisplay.java
+++ app/src/main/java/tud/tk3/ambientdisplay/app/communication/AmbientDisplay.java
@@ -2,11 +2,19 @@ package tud.tk3.ambientdisplay.app.communication;
 
 import android.app.Activity;
 
+import java.util.List;
+
+import tud.tk3.ambientdisplay.app.display.ImageSection;
+
 /**
  * Created by martin on 21.05.14.
  */
 public interface AmbientDisplay {
 
     public void displayImage(byte[] data);
+
+    public void topologyChange(List<ImageSection> sections);
+
+    public void imageSectionChange(ImageSection section);
 
 }
